ec4ecfa6b7817c4e7cfd77230ab30d863054c1c4
AdwaithVS <adwaithvs512@gmail.com>
2024-08-10 11:47:45 +0530

    search todo page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import AddToDo from './components/AddToDo';
+import SearchToDo from './components/SearchToDo';
 
 function App() {
   return (
-    <AddToDo />
+    <SearchToDo/>
   );
 }
 
